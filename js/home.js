504e1e260497b1f1d9c6efe09885d959e0b19d42
oh2k1vn <oh2k1vn@gmail.com>
2022-04-04 22:52:29 +0700

    hieu

---
+++ js/home.js
@@ -0,0 +1,18 @@
+// Menu Search
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// menu
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
