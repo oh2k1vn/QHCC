2dfd591f7a77f99b86289a21630f0d18554202d7
oh2k1vn <oh2k1vn@gmail.com>
2022-04-05 13:33:28 +0700

    hieu

---
+++ javascrip.js
@@ -0,0 +1,61 @@
+  // Add active class to the current button (highlight it)
+  var header = document.getElementById("menu");
+  var btns = header.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  // Chat box
+  /*Hàm Mở Form*/
+  function moForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  /*Hàm Đóng Form*/
+  function dongForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 700 ||
+      document.documentElement.scrollTop > 700
+    ) {
+      document.getElementById("header").style.background = "#111";
+    } else {
+      document.getElementById("header").style.background = "transparent";
+    }
+  }
