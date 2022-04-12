bdfa57cea8fe0f452d63e79568c8e20ecd98f0d7
oh2k1vn <oh2k1vn@gmail.com>
2022-04-12 23:39:59 +0700

    chot don

--- assets/js/javascrip.js
+++ assets/js/javascrip.js
@@ -1,4 +1,3 @@
-// Add active class to the current button (highlight it)
 var header = document.getElementById("menu");
 var btns = header.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
@@ -8,21 +7,11 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
-
-// Get the modal
 var modal = document.getElementsByClassName("modal");
-
-// Get the button that opens the modal
 var modal_btn = document.getElementsByClassName("myBtn");
-
-// Get the <span> element that closes the modal
 var span_close = document.getElementsByClassName("close");
-
-// Get left and right arrows
 var left = document.getElementsByClassName("left");
 var right = document.getElementsByClassName("right");
-
-// set data indexes on errrythang.
 function setDataIndex() {
   for (i = 0; i < modal_btn.length; i++) {
     modal_btn[i].setAttribute("data-index", i);
@@ -32,77 +21,58 @@ function setDataIndex() {
     right[i].setAttribute("data-index", i);
   }
 }
-
-//add onclicks to all the model buttons and display models when clicked
 for (i = 0; i < modal_btn.length; i++) {
   modal_btn[i].onclick = function () {
     var ElementIndex = this.getAttribute("data-index");
     modal[ElementIndex].style.display = "block";
     document.body.classList.add("noScroll");
   };
-
-  // When the user clicks on <span> (x), close the modal
   span_close[i].onclick = function () {
     var ElementIndex = this.getAttribute("data-index");
     modal[ElementIndex].style.display = "none";
     document.body.classList.remove("noScroll");
   };
-
   right[i].onclick = function () {
     var ElementIndex = this.getAttribute("data-index");
     var CurrentModal = parseInt(ElementIndex, 10);
-
     if (CurrentModal === modal_btn.length - 1) {
       var NextModal = 0;
     } else {
       var NextModal = CurrentModal + 1;
     }
-
     modal[CurrentModal].style.display = "none";
     modal[NextModal].style.display = "block";
   };
-
   left[i].onclick = function () {
     var ElementIndex = this.getAttribute("data-index");
     var CurrentModal = parseInt(ElementIndex, 10);
-
     if (CurrentModal === 0) {
       var PreviousModal = modal_btn.length - 1;
     } else {
       var PreviousModal = CurrentModal - 1;
     }
-
     modal[PreviousModal].style.display = "block";
     modal[CurrentModal].style.display = "none";
   };
 }
-
 window.onload = function () {
   setDataIndex();
 };
-
-// close the modal when you click outside the box
 window.onclick = function (event) {
   if (event.target === modal[event.target.getAttribute("data-index")]) {
     modal[event.target.getAttribute("data-index")].style.display = "none";
     document.body.classList.remove("noScroll");
   }
 };
-
-// Chat box
-/*Hàm Mở Form*/
 function moForm() {
   document.getElementById("myForm").style.display = "block";
 }
-/*Hàm Đóng Form*/
 function dongForm() {
   document.getElementById("myForm").style.display = "none";
 }
-
 window.onscroll = function () {
   scrollFunction();
 };
-
 function scrollFunction() {
   if (
     document.body.scrollTop > 600 ||
@@ -113,38 +83,24 @@ function scrollFunction() {
     document.getElementById("header").style.background = "transparent";
   }
 }
-
-// lấy phần Modal
 var modalLogin = document.getElementById("myModal_login");
 console.log(modalLogin);
-// Lấy phần button mở Modal
 var btnLogin = document.getElementById("myBtn_login");
-
-// Lấy phần span đóng Modal
 var spanLogin = document.getElementsByClassName("close_login")[0];
-
-// Khi button được click thi mở Modal
 btnLogin.onclick = function () {
   modalLogin.style.display = "block";
 };
-
-// Khi span được click thì đóng Modal
 spanLogin.onclick = function () {
   modalLogin.style.display = "none";
 };
-
-// Khi click ngoài Modal thì đóng Modal
 window.onclick = function (event) {
   if (event.target == modal) {
     modalLogin.style.display = "none";
   }
 };
-
-// search
 function openSearch() {
   document.getElementById("myOverlay").style.display = "block";
 }
-
 function closeSearch() {
   document.getElementById("myOverlay").style.display = "none";
 }
